ee26f2e23416e2a614fc90dfd0fb11dfe08aa3dc
strifeja <strifeja@mail.uc.edu>
2025-06-17 13:24:06 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './Pages/Home'
+import { NotFound } from './Pages/NotFound'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ProjectsSection.jsx
@@ -0,0 +1,113 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+
+const projects = [
+    {
+        id: 1,
+        title: "HealthyPlate",
+        description: 
+            "HealthyPlate is a web application that uses AI to formulate a well balanced meal plan based on user interests.",
+        image: "/projects/project1.png",
+        tags: ["HTML", "CSS", "JavaScript"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+    {
+        id: 2,
+        title: "Flapping-Wing Micro Air Vehicle Simulation Support Software",
+        description: 
+            "Simulation support software with a fully functional GUI that simulates a FWMAV",
+        image: "/projects/project2.png",
+        tags: ["OpenGL", "C++", "CAD"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+    {
+        id: 3,
+        title: "1D Pong",
+        description: "1D pong game in which a “ball” will travel back and forth between two players using a shift register",
+        image: "/projects/project3.png",
+        tags: ["Electronics"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+    
+];
+
+
+export const ProjectsSection = () => {
+    return (
+        <section id="projects" className="py-24 px-4 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> 
+                    Featured <span className="text-primary"> Projects </span>
+                </h2>
+
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    Here are some of my recent projects. Each project was carefully 
+                    crafted with attention to detail, performance, and user experience.
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {projects.map((project, key) => (
+                        <div 
+                            key={key} 
+                            className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+                        >
+                            <div className="h-48 overflow-hidden">
+                                <img 
+                                    src={project.image}
+                                    alt={project.title} 
+                                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                                />
+                            </div>
+
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {project.tags.map((tag) => (
+                                        <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary/30 text-secondary-foreground">
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+
+
+                                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                                <p className="text-muted-foreground text-sm mb-4"> {project.description}</p>
+                                
+                                <div className="felx justify-between items-center">
+                                    <div className="flex space-x-3">
+                                        <a 
+                                            href={project.demoUrl} 
+                                            target="_blank"
+                                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                        > 
+                                            <ExternalLink size={20}/> 
+                                        </a>
+                                        <a 
+                                            href={project.githubUrl} 
+                                            target="_blank"
+                                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                        > 
+                                            <Github size={20}/> 
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="text-center mt-12">
+                    <a 
+                        className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                        target="_blank"
+                        href="#"
+                    >
+                        Check My Github <ArrowRight size={16}/>
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+};
