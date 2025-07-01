5b76e58ce4386d14bb1fc86312a4cc7ea0b2fccb
strifeja <strifeja@mail.uc.edu>
2025-07-01 13:39:59 -0400

    Redesign Test - Remove HeroSection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,28 @@
-import { ThemeToggle } from "../Components/ThemeToggle";
-import { StarBackground } from "../Components/StarBackground";
-import { Navbar } from "../Components/Navbar";
-import { HeroSection } from "../Components/HeroSection";
 import { AboutSection } from "../Components/AboutSection";
-import { SkillsSection } from "../Components/SkillsSection";
-import { ProjectsSection } from "../Components/ProjectsSection";
 import { ContactSection } from "../Components/ContactSection";
 import { Footer } from "../Components/Footer";
+import { Navbar } from "../Components/Navbar";
+import { ProjectsSection } from "../Components/ProjectsSection";
+import { SkillsSection } from "../Components/SkillsSection";
+import { ThemeToggle } from "../Components/ThemeToggle";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        {/* Navbar */}
-        <Navbar />
-        {/* Theme Toggle and Background Effects*/}
-        <ThemeToggle />
-        {/* Main Content */}
-        <main>
-            <HeroSection />
-            <AboutSection />
-            <SkillsSection />
-            <ProjectsSection />
-            <ContactSection />
-        </main>
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Navbar */}
+      <Navbar />
+      {/* Theme Toggle and Background Effects*/}
+      <ThemeToggle />
+      {/* Main Content */}
+      <main>
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
 
-        {/* Footer */}
-        <Footer />
-    </div>;
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 };
